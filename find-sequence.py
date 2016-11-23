ae288543d1f86783373d43705c980efa3926f6e9
Patrick Armitage <armitage.patrick@gmail.com>
2016-11-22 21:09:29 -0600

    Fix find-sequence.py solution

--- find-sequence.py
+++ find-sequence.py
@@ -19,94 +19,51 @@ def horizontal_match(matrix):
 
     return match_found
 
-def vertical_match(matrix):
-    length = len(matrix)
-
+def vertical_match(mtrx):
+    length = len(mtrx)
     match_found = False
-    matches_found = 0
-    curr_num = matrix[0][0]
-    i = 1
-    for col in range(length):
-        while i < length:
-            if curr_num == matrix[i][col]:
-                matches_found += 1
-                if matches_found == 3:
+
+    for y in range(length):
+        for x in range(length):
+            # check upward
+            if (y-3) >= 0:
+                if mtrx[y][x] == mtrx[y-1][x] == mtrx[y-2][x] == mtrx[y-3][x]:
                     match_found = True
-            else:
-                if matches_found > 0:
-                    matches_found = 0
-            i += 1
+            # check downward
+            if (y+3) < length:
+                if mtrx[y][x] == mtrx[y+1][x] == mtrx[y+2][x] == mtrx[y+3][x]:
+                    match_found = True
 
     return match_found
 
-def diagonal_match(matrix):
-    length = len(matrix)
-
+def diagonal_match(mtrx):
+    length = len(mtrx)
     match_found = False
-    matches_found = 0
-    row_num = 0
-    i = 0
-    # check left-to-right matches first
-    curr_num = matrix[0][0]
-    iterrow = iter(range(length))
-    itercol = iter(range(length))
-    # next(iterrow)
-    # next(itercol)
-    for row in iterrow:
-        for col in itercol:
-            while (i+row) < length and (i+col) < length:
-                if curr_num == matrix[i][i+col]:
-                    matches_found += 1
-                    if matches_found == 3:
-                        match_found = True
-                else:
-                    if matches_found > 0:
-                        matches_found = 0
-                curr_num = matrix[i][i+col]
-                i += 1
-            i = 0
 
-            curr_num = matrix[row][col]
-            while (i+row) < length and (i+col) < length:
-                if curr_num == matrix[i][i]:
-                    matches_found += 1
-                    if matches_found == 3:
-                        match_found = True
-                else:
-                    if matches_found > 0:
-                        matches_found = 0
-                curr_num = matrix[i][i]
-                i += 1
-            i = 0
-
-    # then right-to-left
-    i = length - 2
-    j = 1
-    curr_num = matrix[0][length - 1]
-    for row in range(length):
-        while i >= 0:
-            if curr_num == matrix[j][i]:
-                matches_found += 1
-                if matches_found == 4:
+    for y in range(length):
+        for x in range(length):
+            # check upper-left
+            if (x-3) >= 0 and (y-3) >= 0:
+                if mtrx[y][x] == mtrx[y-1][x-1] == mtrx[y-2][x-2] == mtrx[y-3][x-3]:
                     match_found = True
-            else:
-                if matches_found > 0:
-                    matches_found = 0
-            curr_num = matrix[j][i]
-            i -= 1
-            j += 1
-            if j == length:
-                break
-        i = length - 1
-        j = row
-        matches_found = 0
-        curr_num = matrix[j][i]
+            # check upper-right
+            if (x+3) < length and (y-3) >= 0:
+                if mtrx[y][x] == mtrx[y-1][x+1] == mtrx[y-2][x+2] == mtrx[y-3][x+3]:
+                    match_found = True
+            # check lower-left
+            if (x-3) >= 0 and (y+3) < length:
+                if mtrx[y][x] == mtrx[y+1][x-1] == mtrx[y+2][x-2] == mtrx[y+3][x-3]:
+                    match_found = True
+            # check lower-right
+            if (x+3) < length and (y+3) < length:
+                if mtrx[y][x] == mtrx[y+1][x+1] == mtrx[y+2][x+2] == mtrx[y+3][x+3]:
+                    match_found = True
 
     return match_found
 
 def checkio(matrix):
     match = False
-    if horizontal_match(matrix) or vertical_match(matrix) or diagonal_match(matrix):
+    if horizontal_match(matrix) or vertical_match_2(matrix) or diagonal_match_2(matrix):
         match = True
     return match
 
@@ -241,4 +198,27 @@ print(checkio([
     [2, 5, 7, 9, 7, 5, 8, 6],
     [1, 5, 3, 7, 2, 1, 6, 3],
     [1, 1, 3, 7, 7, 4, 9, 7]
+]), True)
+print(checkio([
+    [2, 6, 2, 3, 5, 2, 4, 8, 7],
+    [5, 7, 8, 5, 9, 5, 7, 3, 4],
+    [6, 4, 1, 2, 1, 6, 5, 8, 5],
+    [9, 3, 1, 3, 5, 4, 6, 4, 8],
+    [9, 6, 6, 8, 1, 9, 1, 2, 1],
+    [5, 5, 5, 8, 6, 5, 3, 2, 5],
+    [7, 5, 2, 9, 2, 9, 8, 2, 5],
+    [5, 8, 1, 9, 1, 2, 6, 2, 2],
+    [7, 5, 3, 6, 1, 6, 9, 5, 9]
+]), True)
+print(checkio([
+    [1, 1, 7, 2, 8, 1, 9, 2, 6, 2],
+    [3, 7, 6, 6, 7, 4, 7, 5, 3, 7],
+    [2, 4, 8, 5, 3, 9, 4, 9, 4, 4],
+    [3, 1, 4, 9, 8, 9, 5, 3, 6, 8],
+    [4, 5, 6, 4, 7, 3, 9, 1, 9, 9],
+    [7, 7, 6, 4, 8, 9, 2, 3, 5, 7],
+    [9, 1, 2, 1, 1, 7, 9, 2, 6, 4],
+    [5, 7, 6, 9, 5, 3, 5, 4, 4, 9],
+    [5, 4, 7, 8, 9, 3, 1, 4, 5, 1],
+    [9, 5, 7, 9, 4, 7, 4, 5, 8, 8]
 ]), True)
